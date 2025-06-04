f4ccdd6a8e4b039bcdbdfe742a24e9656985f59e
viktor <kn1b21.kniahnytskyi@kpnu.edu.ua>
2025-06-04 17:15:29 +0300

    update BASE_URL to production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
 // export const BASE_URL = "http://localhost:3001/api";
-const BASE_URL = "https://souvenir-shop-sald.onrender.com/api";
-
+export const BASE_URL = "https://souvenir-shop-sald.onrender.com/api";
