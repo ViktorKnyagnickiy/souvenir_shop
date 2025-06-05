d0fcff2c4816a8bcede493f41aa5bc894337da0e
viktor <kn1b21.kniahnytskyi@kpnu.edu.ua>
2025-06-05 15:53:02 +0300

    update mobil adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,38 +6,23 @@ import { ROUTES } from '../../utils/routes';
 
 import LOGO from '../../images/logo6.png';
 
-
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>
-        <Link to={ROUTES.HOME}>
-          <img src={LOGO} alt="Souvenir" />
+      <div className={styles.top}>
+        <div className={styles.logo}>
+          <Link to={ROUTES.HOME}>
+            <img src={LOGO} alt="Souvenir" />
+          </Link>
+        </div>
+
+        <Link to={ROUTES.CART} className={styles.cart}>
+          Зв'язатись з нами
         </Link>
       </div>
 
       <div className={styles.info}>
-        <form className={styles.form}>
-          {/* <div className={styles.icon}>
-            <SearchIcon className={styles['icon-img']} />
-          </div> */}
-          {/* <div className={styles.input}>
-            <input
-              type="search"
-              name="search"
-              placeholder="Пошук товарів"
-              autoComplete="off"
-              onChange={() => {}}
-              value=""
-            />
-          </div> */}
-          {false && <div className={styles.box}></div>}
-        </form>
 
-        {/* Замість іконки Cart виводимо надпис */}
-        <Link to={ROUTES.CART} className={styles.cart}>
-          Зв'язатись з нами
-        </Link>
       </div>
     </div>
   );
